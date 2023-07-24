15df915f52e906eba98a2a93f8f273894f04b4bf
Trevor Knight <trevorknight17@gmail.com>
2023-07-23 21:42:40 -0400

    patch 2 setup

--- version_info.m
+++ version_info.m
@@ -2,11 +2,11 @@ function version_info
 
 %%% Displays info related to current version
 
-version.typ = ' Release';
+version.typ = ' Develop';
 version.ind = ' V ';
 version.maj = '1';
 version.min = '0';
-version.pat = '1';
+version.pat = '2';
 version.upd = char(datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss Z'));
 
 details = strcat('Current Version:',version.typ,version.ind,version.maj,'.',version.min,'.',version.pat);
